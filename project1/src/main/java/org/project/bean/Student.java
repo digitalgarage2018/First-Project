c974023ac02fd40c486cc51b23eaeb5d86268078
unknown <s216551studenti.polito.it>
2018-04-24 15:38:30 +0200

    Piano di studi e studente bean

---
+++ project1/src/main/java/org/project/bean/Student.java
@@ -0,0 +1,55 @@
+package org.project.bean;
+
+import java.util.Date;
+
+public class Student {
+	private long nMatricola;
+	private String nome;
+	private String cognome;
+	private String email;
+	private Date dataNascita;
+	private long pianoId;
+	
+	public Student(long nMatricola, String nome, String cognome, String email, Date dataNascita, long pianoId) {
+		this.nMatricola = nMatricola;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.email = email;
+		this.dataNascita = dataNascita;
+		this.pianoId=pianoId;
+	}
+	
+	public long getnMatricola() {
+		return nMatricola;
+	}
+	public void setnMatricola(long nMatricola) {
+		this.nMatricola = nMatricola;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getDataNascita() {
+		return dataNascita;
+	}
+	public void setDataNascita(Date dataNascita) {
+		this.dataNascita = dataNascita;
+	}
+	
+}
+	
+	
