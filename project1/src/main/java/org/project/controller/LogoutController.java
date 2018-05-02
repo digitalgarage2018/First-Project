797a0ac7a197fcb1e8800ae25b874809688e6edd
unknown <s216551studenti.polito.it>
2018-05-02 14:58:01 +0200

    logoutcontroller modified

---
+++ project1/src/main/java/org/project/controller/LogoutController.java
@@ -0,0 +1,22 @@
+package org.project.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogoutController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		if(session != null)
+		    session.invalidate();
+		request.getRequestDispatcher("/index.jsp").forward(request,response);
+	}
+}
